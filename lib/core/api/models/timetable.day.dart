dc0f855cd666c28e5435fb449e5ab7819fd31c91
Dev Kev <69854056+DevKevYT@users.noreply.github.com>
2021-12-22 18:13:14 +0100

    Wochentag Name Abkürzung
    kann über getShortName() aufgerufen werden

--- lib/core/api/models/timetable.day.dart
+++ lib/core/api/models/timetable.day.dart
@@ -3,8 +3,11 @@ import 'utils.dart' as utils;
 
 class TimeTableDay {
   final DateTime _date;
+
+  ///Jeder Tag hat 8 Tage fest.
   final _hours = <TimeTableHour>[];
   String _dayName = "";
+  String _shortDayName = "";
 
   int daysSinceEpoch = 0;
   bool outOfScope = false;
@@ -13,24 +16,31 @@ class TimeTableDay {
     switch (_date.weekday) {
       case 1:
         _dayName = "Montag";
+        _shortDayName = "Mo";
         break;
       case 2:
         _dayName = "Dienstag";
+        _shortDayName = "Di";
         break;
       case 3:
         _dayName = "Mittwoch";
+        _shortDayName = "Mi";
         break;
       case 4:
         _dayName = "Donnerstag";
+        _shortDayName = "Do";
         break;
       case 5:
         _dayName = "Freitag";
+        _shortDayName = "Fr";
         break;
       case 6:
         _dayName = "Samstag";
+        _shortDayName = "Sa";
         break;
       case 7:
         _dayName = "Sonntag";
+        _shortDayName = "So";
         break;
       default:
         "";
@@ -56,7 +66,11 @@ class TimeTableDay {
     return _dayName;
   }
 
-  void addHour(dynamic data) {
+  String getShortName() {
+    return _shortDayName;
+  }
+
+  void insertHour(dynamic data) {
     _hours.add(TimeTableHour(data));
 
     _hours.sort((a, b) => a.end.hour.compareTo(b.end.hour));
