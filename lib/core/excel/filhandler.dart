0a49bc7f2ed7fc511eec792a4f8401f33b19adc3
Dev Kev <69854056+DevKevYT@users.noreply.github.com>
2021-12-26 17:55:59 +0100

    Not longer needed

--- lib/core/excel/filhandler.dart
+++
@@ -1,7 +0,0 @@
-//Dieses Objekt ist dafür zuständig eine Excel Datei einzulesen
-
-class FileHandler {
-  static void readFile({String path = ""}) {
-    if (path.isEmpty) throw Exception("Der Pfad existiert nicht");
-  }
-}
