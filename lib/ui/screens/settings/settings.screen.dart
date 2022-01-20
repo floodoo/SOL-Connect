bd79b84e2bc3d1a4bf4e77a92f0fa4d530df9fb8
DevKevYT <devkev@ddbot.top>
2022-01-20 14:24:40 +0100

    update build version info

--- lib/ui/screens/settings/settings.screen.dart
+++ lib/ui/screens/settings/settings.screen.dart
@@ -242,7 +242,7 @@ class SettingsScreen extends ConsumerWidget {
               ),
               padTop: 10,
               padBottom: 30,
-              text: "Version 1.0.0",
+              text: "Version Alpha 1.0.1",
             ),
             Center(
               child: RichText(
