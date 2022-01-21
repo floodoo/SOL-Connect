13525cfa420ed308b92fbea6a590c4dd7f713f7f
Florian Schmitz <schmitz.florian.daniel+github@gmail.com>
2022-01-21 23:34:25 +0100

    #80 load, save and change server address

---
+++ lib/core/service/settings.service.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SettingsService with ChangeNotifier {
+  bool showDeveloperOptions = false;
+  String serverAddress = "flo-dev.me";
+
+  void toggleDeveloperOptions() {
+    showDeveloperOptions = showDeveloperOptions ? false : true;
+    notifyListeners();
+  }
+
+  Future<void> saveServerAdress(String serverAddress) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("serverAddress", serverAddress);
+    this.serverAddress = serverAddress;
+    notifyListeners();
+  }
+
+  void loadServerAdress() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    serverAddress = prefs.getString("serverAddress") ?? "";
+    notifyListeners();
+  }
+}
