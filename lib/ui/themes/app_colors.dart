aa6a2d7edd0e00a76c3e0bb1a61e27bbc01e8432
DevKevYT <devkev@ddbot.top>
2022-01-26 15:33:02 +0100

    #112 refactoring

--- lib/ui/themes/app_colors.dart
+++ lib/ui/themes/app_colors.dart
@@ -105,7 +105,6 @@ class AppColors {
         irregular: Colors.blue.shade900,
         cancelled: const Color(0xffa545b5),
         noTeacher: const Color(0xffa545b5));
-        
   }
 
   factory AppColors.dark() {
