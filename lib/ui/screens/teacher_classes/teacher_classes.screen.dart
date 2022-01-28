eb9a502c3ab5f838719cbb7f8fcf5576a2bed527
Florian Schmitz <schmitz.florian.daniel+github@gmail.com>
2022-01-28 12:11:57 +0100

    #132 add classes as teacher list

--- lib/ui/screens/teacher_classes/teacher_classes.screen.dart
+++ lib/ui/screens/teacher_classes/teacher_classes.screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_search_bar/flutter_search_bar.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:logger/logger.dart';
+import 'package:sol_connect/core/service/services.dart';
 import 'package:sol_connect/util/logger.util.dart';
 
 class TeacherClassesScreen extends ConsumerStatefulWidget {
@@ -26,7 +27,7 @@ class _TeacherClassesScreenState extends ConsumerState<TeacherClassesScreen> {
       },
       buildDefaultAppBar: (BuildContext context) {
         return AppBar(
-          title: const Text('My Home Page'),
+          title: const Text("Ihre Klassen"),
           actions: [searchBar.getSearchAction(context)],
         );
       },
@@ -34,12 +35,46 @@ class _TeacherClassesScreenState extends ConsumerState<TeacherClassesScreen> {
     super.initState();
   }
 
+  Future<List<Widget>> buildAllTeacherClasses() async {
+    List<Widget> list = [];
+    final classesAsTeacher = await ref.read(timeTableService).session.getClassesAsTeacher();
+
+    for (var i = 0; i < classesAsTeacher.length; i++) {
+      list.add(
+        ListTile(
+          title: Text(classesAsTeacher[i].name),
+          subtitle: Text(classesAsTeacher[i].classTeacherName),
+          onTap: () {},
+        ),
+      );
+      if (i == classesAsTeacher.length) {
+        list.add(
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: Divider(
+              color: ref.watch(themeService).theme.colors.textInverted,
+            ),
+          ),
+        );
+      }
+    }
+
+    return list;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: searchBar.build(context),
-      body: const Center(
-        child: Text("Teacher Classes Screen"),
+      body: FutureBuilder(
+        future: buildAllTeacherClasses(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            return ListView(children: snapshot.data);
+          }
+        },
       ),
     );
   }
