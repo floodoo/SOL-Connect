e98dce25f07dff8e567aa7e3694a0900dfe77a80
Florian Schmitz <schmitz.florian.daniel+github@gmail.com>
2022-04-25 11:32:51 +0200

    update version

--- lib/ui/screens/settings/settings.screen.dart
+++ lib/ui/screens/settings/settings.screen.dart
@@ -419,7 +419,7 @@ class SettingsScreen extends ConsumerWidget {
                       ),
                       padTop: 10,
                       padBottom: 15,
-                      text: "Version Alpha 1.0.1",
+                      text: "Version 1.1.0+2",
                     ),
                     const DeveloperOptions(),
                   ],
